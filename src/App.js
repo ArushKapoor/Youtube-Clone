428377a900c749dfe90847582c680c4535d9f305
ArushKapoor <arush0113.cse19@chitkara.edu.in>
2020-08-26 16:24:21 +0530

    added sidebar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import Header from './Header';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import Sidebar from './Sidebar';
+import RecommendedVideos from "./RecommendedVideos";
+import "./App.css";
 
 function App() {
   return (
     // BEM class naming convention
     <div className="app">
       <Header />
-
-      {/* Sidebar */}
-      {/* Recommended Video */}
+      <div className="app__page">
+        <Sidebar />
+        <RecommendedVideos />
+      </div>
     </div>
   );
 }
